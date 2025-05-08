109ddb5b505e28bc8d74cc58d9d3df56ef744af4
Karthiksankarfd <karthikeyansankar887@gmail.com>
2025-05-09 03:59:11 +0530

    changed db connection string

--- server/config/db.js
+++ server/config/db.js
@@ -1,15 +1,17 @@
 require("dotenv").config()
 const mongoose  = require("mongoose")
 const db_name = process.env.DATA_BASE_NAME;
-const user_name = process.env.USER_NAME 
+const user_name = encodeURIComponent(process.env.USER_NAME )
 const password = process.env.DB_PASSSWORD 
-const uri = `mongodb+srv://${user_name}:${password}@cluster-one.buuxkm3.mongodb.net/${db_name}?retryWrites=true&w=majority&appName=Cluster-one`
-const connectToMongo = async (req , res)=>{
+// const uri = `mongodb+srv://${user_name}:${password}@cluster-one.buuxkm3.mongodb.net/${db_name}?retryWrites=true&w=majority&appName=Cluster-one`
+// const uri = `mongodb+srv${user_name}://:${password}@cluster-one.buuxkm3.mongodb.net/${db_name}?retryWrites=true&w=majority&appName=Cluster-one`
+
+ const uri = "mongodb+srv://karthik:p10MXExNf0EBSnks@cluster-one.buuxkm3.mongodb.net/meetx?retryWrites=true&w=majority&appName=Cluster-one"
+const connectToMongo = async ()=>{
     try{
        await mongoose.connect(uri)
     }catch(e){
-        console.log("CANNOT CONNECT TO DB" , e)
-       
+        console.log("CANNOT CONNECT TO DB" , e.message)
     }
 }
 
